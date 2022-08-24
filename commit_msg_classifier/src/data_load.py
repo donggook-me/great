62f253106c64409c020a679c6ea7b6d7cd6dfe05
donggook-me <ert1306@naver.com>
2022-08-23 23:14:26 -0700

    add docstring and remove mess by dongguk

--- commit_msg_classifier/src/data_load.py
+++ commit_msg_classifier/src/data_load.py
@@ -2,18 +2,6 @@ import os
 import re
 import string
 import math
-# DATA_DIR = 'enron'
-target_names = ["adjustedszz_bugfix",
-    "issueonly_bugfix",
-    "testchange_javacode",
-    "documentation_technicaldept_add",
-    "refactoring_codebased",
-    "documentation_technicaldept_remove",
-    "refactoring_keyword",
-    "documentation_javainline",
-    "documentation_javadoc",
-    "issueonly_featureadd",
-    "validated_bugfix"]
 
 def get_data(msg_nl, class_label_csv):
     data = []
@@ -40,13 +28,12 @@ def get_data(msg_nl, class_label_csv):
         fc.close()
     return data, target
     # data = ["this is normal commit", "to change version"...]
-    # target = [[0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0], [1,0,0,0,0].....]
+    # target = [8,2,0,2,2,2,2,2,4,2,2,2.....] number means index of target_names
 
 def main():
-    X, y = get_data("input/msg.nl", "input/input.csv")
+    X, y = get_data("../input/msg.nl", "../input/input.csv")
     print("len(x)", len(X))
     print("len(Y)", len(y))
-    print("y: ", y[:300])
 
 if __name__ == '__main__':
     main()
--- commit_msg_classifier/src/main.py
+++ commit_msg_classifier/src/main.py
@@ -1,15 +1,15 @@
-from  data_load import *
-from helper import *
+from data_load import *
+from msg_detector import *
 
 
 if __name__ == '__main__':
-    X, y = get_data("input/msg.nl", "input/input.csv")
-    # print("test", X[:10], y[:10])
+    X, y = get_data("../input/msg.nl", "../input/input.csv")
     MNB = msgDetector()
     MNB.fit(X[500:], y[500:])
     pred = MNB.predict(X[:500])
     true = y[:500]
-    print("pred", pred)
-    print("true",true)
+    print("pred: ", pred)
+    print()
+    print("true: ",true)
     accuracy = sum(1 for i in range(len(pred)) if pred[i] == true[i]) / float(len(pred))
-    print("{0:.4f}".format(accuracy))
+    print("\n{0:.4f}".format(accuracy))
